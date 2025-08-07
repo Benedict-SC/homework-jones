1ebe4fc0f95bab35887f0b2d5f8368802be8e7fe
nightpool <nightpool@cybre.space>
2025-08-06 23:12:57 -0700

    HWJ tweaks

--- homework-jones/scripts/battle/enemies/hjones.lua
+++ homework-jones/scripts/battle/enemies/hjones.lua
@@ -2,66 +2,62 @@ local HJones, super = Class(EnemyBattler)
 
 function HJones:init()
     super.init(self)
-
-    -- Enemy name
     self.name = "Homework Jones"
+
     -- Sets the actor, which handles the enemy's sprites (see scripts/data/actors/dummy.lua)
     self:setActor("homework")
 
-    -- Enemy health
     self.max_health = 4000
     self.health = 4000
-    -- Enemy attack (determines bullet damage)
     self.attack = 9
-    -- Enemy defense (usually 0)
     self.defense = 0
-    -- Enemy reward
     self.money = 100
-
-    -- Mercy given when sparing this enemy before its spareable (20% for basic enemies)
-    self.spare_points = 20
 
     -- List of possible wave ids, randomly picked each turn
     self.waves = {
         "essay",
+        "aiming",
     }
 
     -- Dialogue randomly displayed in the enemy's speech bubble
     self.dialogue = {
         "Kya ha ha!",
         "I'm Homework Jones!",
-        "Solve my puzzles\nand problems!",
-        "I before E except in\na lot of situations!"
+        "Solve my puzzles and problems!",
+        "I before E except in a lot of situations!"
     }
 
     -- Check text (automatically has "ENEMY NAME - " at the start)
-    self.check = "AT 9 DF 0\n* Miss Alphys has created a\n* monster. I mean Darkner."
+    self.check = "AT 9 DF 0\n"..
+        "* Miss Alphys has created a\n"..
+        "* monster. I mean Darkner."
 
     -- Text randomly displayed at the bottom of the screen each turn
     self.text = {
-        "* Homework Jones assigns you\nthree to four hours\nof mandatory fun.",
-        "* Homework Jones wants to \nknow how your college\napplications are going.",
-        "* Homework Jones is attempting\nto make economics 'Kool.'",
-        "* Homework Jones is teaching\nyou a mnemonic that\ndoesn't stand for anything.",
-        "* Looking at Homework Jones,\nyou remembered what\na mitochondria was.",
-        "* Normal to want.\nPossible to achieve.",
+        "* Homework Jones assigns you three to four hours of mandatory fun.",
+        "* Homework Jones wants to know how your college applications are going.",
+        "* Homework Jones is attempting to make economics 'Kool.'",
+        "* Homework Jones is teaching you a mnemonic that doesn't stand for anything.",
+        "* Looking at Homework Jones, you remembered what a mitochondria was.",
+        "* Normal to want. Possible to achieve.",
     }
+
     -- Text displayed at the bottom of the screen when the enemy has low health
-    self.low_health_text = "* Homework Jones tries to\nprove there's an afterlife\nwith basic algebra."
+    self.low_health_text = "* Homework Jones tries to prove there's an afterlife with basic algebra."
 
     -- Register act called "Smile"
     self:registerAct("EatHW")
-    -- Register party act with Ralsei called "Tell Story"
-    -- (second argument is description, usually empty)
-    self:registerAct("EatHWX","",{"susie"})
+
+    self:registerAct("EatHWX", "", {"susie"})
     self:registerAct("SolveForX", "", {"ralsei"})
 end
 
 function HJones:onAct(battler, name)
     if name == "EatHW" then
-        self:hurt(100,Game.battle:getPartyBattler("kris"))
+        self:hurt(100, Game.battle:getPartyBattler("kris"))
         -- Change this enemy's dialogue for 1 turn
-        self.dialogue_override = "... ^^"
+        -- self.dialogue_override = "... ^^"
+
         -- Act text (since it's a list, multiple textboxes)
         return {
             "* You smile.[wait:5]\n* The dummy smiles back.",
@@ -70,7 +66,7 @@ function HJones:onAct(battler, name)
 
     elseif name == "SolveForX" then
         -- Loop through all enemies
-        Game.battle:startActCutscene("favoritefriend", "favoriteFriend",self)
+        Game.battle:startActCutscene("favoritefriend", "favoriteFriend")
 
     elseif name == "Standard" then --X-Action
         -- Give the enemy 50% mercy
