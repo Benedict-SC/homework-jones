6f34da3a38df726e80f85efd729e577159a2fc1b
nightpool <nightpool@cybre.space>
2025-08-06 22:53:47 -0700

    Some small formatting tweaks to the dummy.

--- homework-jones/scripts/battle/cutscenes/dummy.lua
+++ homework-jones/scripts/battle/cutscenes/dummy.lua
@@ -3,9 +3,11 @@ return {
     -- This allows it to fetch us useful documentation that shows all of the available cutscene functions while writing our cutscenes!
 
     ---@param cutscene BattleCutscene
+    ---@param battler PartyBattler
+    ---@param enemy EnemyBattler
     susie_punch = function(cutscene, battler, enemy)
         -- Open textbox and wait for completion
-        cutscene:text("* Susie threw a punch at\nthe dummy.")
+        cutscene:text("* Susie threw a punch at the dummy.")
 
         -- Hurt the target enemy for 1 damage
         Assets.playSound("damage")
@@ -14,11 +16,18 @@ return {
         cutscene:wait(1)
 
         -- Susie text
-        cutscene:text("* You,[wait:5] uh,[wait:5] look like a weenie.[wait:5]\n* I don't like beating up\npeople like that.", "nervous_side", "susie")
+        cutscene:text(
+            "* You,[wait:5] uh,[wait:5] look like a weenie.[wait:5]\n"..
+            "* I don't like beating up people like that.",
+            "nervous_side", "susie"
+        )
 
         if cutscene:getCharacter("ralsei") then
             -- Ralsei text, if he's in the party
-            cutscene:text("* Aww,[wait:5] Susie!", "blush_pleased", "ralsei")
+            cutscene:text(
+                "* Aww,[wait:5] Susie!",
+                "blush_pleased", "ralsei"
+            )
         end
     end
 }
